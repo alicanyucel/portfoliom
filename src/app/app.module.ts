e3c477cb70cc54741a4c0e9ace45f306298ef219
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-14 21:24:56 +0300

    nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/components/header/header.component';
+import { NavComponent } from 'src/components/nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
